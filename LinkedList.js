c57a0e35e28244e824719a961b2451b0f91e1cb2
kconst <kconst@gmail.com>
2015-09-30 14:52:23 -0400

    Initial commit

---
+++ LinkedList.js
@@ -0,0 +1,83 @@
+(function(){
+    function LinkedList() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    LinkedList.prototype.push = function(value) {
+        var node = {
+            value : value,
+            next : null
+        }, current;
+
+        if (!this.head) {
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while(current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+
+        this.length += 1;
+    };
+
+    LinkedList.prototype.forEach = function(func) {
+        var count = 0,
+            current = this.head;
+
+        while (count < this.length) {
+            func(current, count);
+            current = current.next;
+            count += 1;
+        }
+    };
+
+    LinkedList.prototype.some = function(func) {
+        var count = 0,
+            current = this.head,
+            truth;
+
+        while (count < this.length && truth !== true) {
+            truth = func(current, count);
+            current = current.next;
+            count += 1;
+        }
+
+
+        return !!truth;
+    };
+
+    LinkedList.prototype.get = function(pos, returnReference) {
+        var current = this.head,
+            count = 0;
+
+        pos = parseInt(pos, 10);
+
+        if (pos > -1 && pos < this.length) {
+            if (pos === 0) {
+                if (returnReference) {
+                    return this.head;
+                } else {
+                    return this.head.value;
+                }
+            }
+
+            while (count < this.length && count !== pos) {
+                current = current.next;
+                count += 1;
+            }
+
+            if (returnReference) {
+                return current;
+            } else {
+                return current.value;
+            }
+        }
+    };
+
+    module.exports = LinkedList;
+}());
